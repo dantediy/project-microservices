4f425b184120917236720fd089f8d330ff7f4ed8
EC2 Default User <ec2-user@ip-172-31-18-17.us-east-2.compute.internal>
2022-07-26 09:30:31 +0000

    run k8s

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="dantediy"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+k run project-microservices\
+    --image=$dockerpath/project-microservices\
+    --labels app=python-microservices
 
+sleep 20;
 
 # Step 3:
 # List kubernetes pods
-
+k get pods
 # Step 4:
 # Forward the container port to a host
 
+k port-forward project-microservices --address 0.0.0.0 8000:80
